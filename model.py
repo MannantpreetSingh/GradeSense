eac65fe86755e3ae9706bc96ecc9d40c76e51529
Mannatpreet Singh <mannatpreetsingh14@outlook.com>
2026-07-28 21:41:13 +0530

    updated dataset

--- model.py
+++ model.py
@@ -2,13 +2,16 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+import pickle
 
 data=pd.read_csv("data.csv")
-x=data[["hours","attendance"]]
+x=data[["hours","attendance","marks"]]
 y=data["result"]
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2)
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2 , random_state=42)
 model=LogisticRegression()
 model.fit(x_train,y_train)
 prediction = model.predict(x_test)
 print("prediction :",prediction)
-print("Accuracy:", accuracy_score(y_test, prediction ))
+print("Accuracy:", accuracy_score(y_test, prediction))
+pickle.dump(model, open("model.pkl", "wb"))
+print("Model saved successfully!")
